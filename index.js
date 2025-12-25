45b594e5db1784974ba31b76e30059e84bc49c3b
tha_nguyen <>
2025-12-25 09:56:16 +0700

    feat: add product id

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ app.get("/api/products", async (req, res) => {
   const products = await Product.find().sort({ createdAt: -1 });
   res.json(products);
 });
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Không tìm thấy sản phẩm" });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // ✅ ADMIN API (có token)
 app.get("/api/admin/banners", async (req, res) => {
